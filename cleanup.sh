b17614f7c2b54fdeab41d10a02886e00af1cfa57
wesley <wjarmon19@gmail.com>
2025-04-18 06:30:12 +0000

    Added cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## When Maker converts pdfs into markdown format it breaks the pdf up and creates a bunch of
+## image files if necessary, and extra folders.  This script cleans all that up by deleting
+## the images, which Onyx can't use, and moving all files up to one central folder
+## so Onyx does not have to look through a complex folder tree for the files.
+
+TARGET_DIR="/home/wesley/docker/onyx/paperless-docs-for-onyx/paperless-markdowns-done" #Should probably match the OUTPUT_DIRECTORY in paperless-markdown-conversion.sh
+
+# Step 1: Remove all image files in TARGET_DIR and subdirectories
+find "$TARGET_DIR" -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.gif" -o -iname "*.bmp" -o -iname "*.svg" \) -exec rm -v {} \;
+
+# Step 2: Move all remaining files to TARGET_DIR
+find "$TARGET_DIR" -type f -exec mv -v {} "$TARGET_DIR" \;
+
+# Step 3: Delete empty directories
+find "$TARGET_DIR" -type d -empty -delete
+
+echo "Cleanup complete."
